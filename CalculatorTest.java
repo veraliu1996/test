66b31d06416ac3458742548b33e0810aff57088e
Vera <a294459960609@gmail.com>
2024-08-03 16:00:01 +0800

    test

---
+++ CalculatorTest.java
@@ -0,0 +1,42 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+    private Calculator calculator;
+
+    @BeforeEach
+    public void setUp() {
+        calculator = new Calculator();
+        System.out.println("Setup for test");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        calculator = null;
+        System.out.println("Cleanup after test");
+    }
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, calculator.add(2, 3));
+        System.out.println("Test add method");
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(1, calculator.subtract(3, 2));
+        System.out.println("Test subtract method");
+    }
+
+    @Test
+    public void testmultiply() {
+        assertEquals(6, calculator.multiply(3, 2));
+        System.out.println("Test multiply method");
+    }
+
+}
+
